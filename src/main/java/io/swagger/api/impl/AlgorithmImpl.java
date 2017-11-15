9c8aff359aac6a1b847f30b5c58ea74d3967f27d
rautenberg <rautenberg@in-silico.ch>
2017-11-15 16:07:35 +0100

    add debugging outputs

--- src/main/java/io/swagger/api/impl/AlgorithmImpl.java
+++ src/main/java/io/swagger/api/impl/AlgorithmImpl.java
@@ -42,10 +42,10 @@ public class AlgorithmImpl extends AlgorithmService {
 
     @Override
     public Response algorithmGet(String accept, String subjectid, SecurityContext securityContext, UriInfo ui) throws NotFoundException, IOException {
-        System.out.println("========================\nbaseuir: " + ui.getBaseUri() + "\n=============================");
+        System.out.println("========================\nbaseuir 1: " + ui.getBaseUri() + "\n=============================");
         String baseuri = ui.getBaseUri().toString();
-        System.out.println("========================\nbaseuir: " + baseuri + "\n=============================");
-        InputStream in = new URL( ui.getBaseUri() + "swagger.json" ).openStream();
+        System.out.println("========================\nbaseuir 2: " + baseuri + "\n=============================");
+        InputStream in = new URL( ui.getBaseUri().toString() + "swagger.json" ).openStream();
         String jsonContent;
         try {
             jsonContent = new String(IOUtils.toString(in, "UTF-8"));
