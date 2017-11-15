c5359e77fafe419e1e75db533f8b05dde0aef738
rautenberg <rautenberg@in-silico.ch>
2017-11-15 17:55:34 +0100

    update API description and swagger ui library

--- src/main/java/io/swagger/api/Bootstrap.java
+++ src/main/java/io/swagger/api/Bootstrap.java
@@ -15,8 +15,11 @@ public class Bootstrap extends HttpServlet {
   @Override
   public void init(ServletConfig config) throws ServletException {
     Info info = new Info()
-      .title("JGU WEKA REST services")
-      .description("REST API webservice for weka.")
+      .title("JGU WEKA REST Service")
+      .description("RESTful API Webservice to WEKA Machine Learning Algorithms.\n" +
+              "This webservice provides an [OpenRiskNet](https://openrisknet.org/) compliant REST interface to machine learning algorithms from the WEKA Java Library.\n" +
+              "This application is developed by the [Institute of Computer Science](http://www.datamining.informatik.uni-mainz.de) at the Johannes Gutenberg University Mainz.\n" +
+              "OpenRiskNet is funded by the European Commission GA 731075. WEKA is developed by the [Machine Learning Group](https://www.cs.waikato.ac.nz/ml/index.html) at the University of Waikato.")
       .termsOfService("")
       .contact(new Contact()
         .email("rautenberg@uni-mainz.de"))
